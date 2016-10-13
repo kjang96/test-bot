b14202e42713b1c073c9129148781a8290cc7fe8
Kathy Jang <kathyjang@berkeley.edu>
2016-10-13 22:05:23 +0100

    more complex reaction to hi or hello

--- index.js
+++ index.js
@@ -30,6 +30,28 @@ controller.on('bot_channel_join', function (bot, message) {
   bot.reply(message, "I'm here!")
 })
 
-controller.hears(['hi'], ['ambient', 'direct_message','direct_mention','mention'], function (bot, message) {
-  bot.reply(message, 'Hello.')
+controller.hears(['hi', 'hello'], 
+  ['ambient', 'direct_message','direct_mention','mention'], 
+  function (bot, message) {
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'robot_face',
+    }, function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
+
+
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message, 'Hello.');
+        }
+    });
+  //bot.reply(message, 'Hello.')
+
 })
+
